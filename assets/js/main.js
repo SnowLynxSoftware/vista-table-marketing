7c6b38bc45c6c387f5a20f795ef41bd1552b4ed3
Dylan Legendre <dylanlegendre09@gmail.com>
2023-10-09 21:25:30 -0500

    finished prototype

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,4 @@
-/**
-* Template Name: Siimple
-* Updated: Sep 18 2023 with Bootstrap v5.3.2
-* Template URL: https://bootstrapmade.com/free-bootstrap-landing-page/
-* Author: BootstrapMade.com
-* License: https://bootstrapmade.com/license/
-*/
-(function() {
+(() => {
   "use strict";
 
   /**
